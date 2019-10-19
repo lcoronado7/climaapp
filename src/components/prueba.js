0aaf13ea314829a6615de48d577dbad333abd37c
lcoronado7 <48112385+lcoronado7@users.noreply.github.com>
2019-10-19 02:57:45 -0400

    Project Update 1

---
+++ src/components/prueba.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class Prueba extends Component{
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default  Prueba;
